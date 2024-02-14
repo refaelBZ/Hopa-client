9fa8cd8ada4384286c8a9a3d3d5a4a786c72dc70
RefaelBZ <refael747@gmail.com>
2024-02-14 17:58:09 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-// import { useEffect, useState } from 'react';
-// import { socket } from './socket';
-import InputMassge from "./InputMassge";
+import { useEffect, useState } from 'react';
+import { socket } from './socket';
 
 export default function App() {
 
 
-  // const [arg, setArg]=useState()
+  const [arg, setArg]=useState()
 
-  // const handleClick = () => {
-  //   socket.emit("clientMessage", "hhgggg");
-  // };
+  const handleClick = () => {
+    socket.emit("clientMessage", "hhgggg");
+  };
 
-  // useEffect(() => {
-  //   socket.on("serverMessage", (arg) => {
-  //     console.log(arg);
-  //     setArg(arg)
+  useEffect(() => {
+    socket.on("serverMessage", (arg) => {
+      console.log(arg);
+      setArg(arg)
 
-  //   });
+      socket.on("msgHistory", (arg) => {
+        console.log(arg);
+        setArg(arg)
+      
+  
+      });
+    
 
-  //   return () => {
-  //     socket.off("serverMessage");
-  //   };
-  // }, [arg]);
+    });
+
+
+    
+
+
+    return () => {
+      socket.off("serverMessage");
+    };
+  }, []);
+
 
   return (
     <div className="App">
-      {/* <button onClick={handleClick}>Send</button>
-      {arg} */}
-      <InputMassge/>
+      <button onClick={handleClick}>Send</button>
+      {arg}
     </div>
   );
 }
