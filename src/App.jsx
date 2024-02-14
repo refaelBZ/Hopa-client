2fb85a63234663e260d78dff77b6ea27a3b6026d
RefaelBZ <refael747@gmail.com>
2024-02-14 14:58:01 +0200

    send option

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { socket } from './socket';
-import { ConnectionState } from './components/ConnectionState';
-import { ConnectionManager } from './components/ConnectionManager';
-import { Events } from "./components/Events";
-import { MyForm } from './components/MyForm';
 
 export default function App() {
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [fooEvents, setFooEvents] = useState([]);
+
+
+  const [arg, setArg]=useState()
+
+  const handleClick = () => {
+    socket.emit("clientMessage", "hhgggg");
+  };
 
   useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
+    socket.on("serverMessage", (arg) => {
+      console.log(arg);
+      setArg(arg)
 
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-
-    function onFooEvent(value) {
-      setFooEvents(previous => [...previous, value]);
-    }
-
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
-    socket.on('foo', onFooEvent);
+    });
 
     return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-      socket.off('foo', onFooEvent);
+      socket.off("serverMessage");
     };
-  }, []);
+  }, [arg]);
 
   return (
     <div className="App">
-      <ConnectionState isConnected={ isConnected } />
-      <Events events={ fooEvents } />
-      <ConnectionManager />
-      <MyForm />
+      <button onClick={handleClick}>Send</button>
+      {arg}
     </div>
   );
 }
